083a67bb25c66ab1246f025f044f428977c3696f
Luis Obis <luis.antonio.obis@gmail.com>
2022-12-08 01:26:24 +0100

    add additional check for files

--- scripts/test_list_matches_tree.py
+++ scripts/test_list_matches_tree.py
@@ -12,24 +12,20 @@ def test_list_matches_tree(list_path: Path, html_path: Path, files_path: Path = 
         data = json.load(f)
 
     urls_from_list = [item["url"] for item in data]
-    print("from list:", urls_from_list)
     with open(html_path, "r") as f:
         content = f.read()
 
     # get all links in the html page
     urls_from_html = re.findall(r"<a href=(.+?)>", content)
-    print("from html:", urls_from_html)
 
     # keep only those matching pattern https://lobis.github.io/gas-files/*.gas.json
     urls_from_html = [url for url in urls_from_html if url.startswith(
         "https://lobis.github.io/gas-files/") and url.endswith(".gas.json")]
 
-    print("from html after:", urls_from_html)
-
     # check the number of files matches the files in files_path
     if files_path is not None:
         files = [file for file in files_path.iterdir() if file.suffix == ".gas"]
-        print(len(files), files)
+        print(len(files), len(urls_from_list), files)
         # assert len(files) == len(urls_from_list)
 
     # check that the number of links in the html page matches the number of urls in the json file
@@ -44,6 +40,9 @@ if __name__ == "__main__":
                         help="Path to json file with list of gas files")
     parser.add_argument("html_path", type=Path,
                         help="Path to html file with tree of gas files")
+    # add optional argument that defaults to None
+    parser.add_argument("--files_path", type=Path, default=None,)
+
     args = parser.parse_args()
 
-    test_list_matches_tree(args.list_path, args.html_path)
+    test_list_matches_tree(args.list_path, args.html_path, args.files_path)
